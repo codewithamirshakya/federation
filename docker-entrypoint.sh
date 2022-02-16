2331141eea78499c8ee94fb00b9dd17d168a432c
Najee Nepali <najeen.nepali@gmail.com>
2022-02-16 11:08:21 +0545

    add entrypoint tocreate keys folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,10 @@ if [[ ! -v PTV_OAUTH_KEY ]];then
     echo -e "OAUTH KEY DOESNOT EXIST"
     exit 1
 fi
+
+if [ ! -d ${OAUTH_PATH} ];then
+    mkdir -p ${OAUTH_PATH}
+fi
  
 echo $PTV_OAUTH_KEY | base64 -d > ${OAUTH_PATH}/oauth-public.key
 exec "$@"

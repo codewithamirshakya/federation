fb9c4789a781f05235e8ddf13441cc3780380903
amir shakya <amir.shakya@diagonal.software>
2022-03-28 12:27:49 +0545

    Default js updated

--- config/default.js
+++ config/default.js
@@ -1,15 +1,12 @@
 const SERVICES = {
-    AUTH: process.env.AUTH_SERVICE || "http://localhost:9999/graphql",
-    CHANNEL: process.env.CHANNEL_SERVICE || "http://localhost:8001/graphql",
-    EMOJI: process.env.EMOJI_SERVICE || "http://localhost:9999/graphql"
-
+    WEB: process.env.WEB_SERVICE || "http://localhost:8001/graphql",
+    WIDGET: process.env.V_SERVICE || "http://localhost:9999/graphql"
 }
 
 
 module.exports = {
     services: [
-        { name: "auth", url: SERVICES.AUTH },
-        { name: "channel", url: SERVICES.CHANNEL },
-        { name: "emoji", url: SERVICES.EMOJI },
+        { name: "web", url: SERVICES.WEB },
+        { name: "widget", url: SERVICES.WIDGET }
     ]
 };

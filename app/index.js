9951dd013b17c90c3e1115d78524aea297c70ffe
amir shakya <amir.shakya@diagonal.software>
2022-03-30 21:31:29 +0545

    Plugin updated

--- app/index.js
+++ app/index.js
@@ -38,9 +38,33 @@ const gateway = new ApolloGateway({
   }
 });
 
+const myPlugin = {
+    // Fires whenever a GraphQL request is received from a client.
+    async requestDidStart(requestContext) {
+      console.log('Request started! Query:\n' +
+        requestContext.request.query);
+  
+      return {
+        // Fires whenever Apollo Server will parse a GraphQL
+        // request to create its associated document AST.
+        async parsingDidStart(requestContext) {
+          console.log('Parsing started!');
+        },
+  
+        // Fires whenever Apollo Server will validate a
+        // request's document AST against your GraphQL schema.
+        async validationDidStart(requestContext) {
+          console.log('Validation started!');
+        },
+  
+      }
+    },
+  };
+
 const server = new ApolloServer({
   gateway,
   subscriptions: false,
+  plugins: [myPlugin],
   context: ({ req }) => {
     const user = req.user || null;
     const auth = req.headers.authorization || null;

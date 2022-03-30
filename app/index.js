ceeea8ef69f760e39fb17322bd57ecc44fcf9f47
amir shakya <amir.shakya@diagonal.software>
2022-03-30 21:49:11 +0545

    Plugin reverted

--- app/index.js
+++ app/index.js
@@ -48,34 +48,11 @@ const gateway = new ApolloGateway({
     }
 });
 
-const gPlugin = {
-    // Fires whenever a GraphQL request is received from a client.
-    async requestDidStart(requestContext) {
-      console.log('Request started! Query:\n' +
-        requestContext.request.query);
-  
-      return {
-        // Fires whenever Apollo Server will parse a GraphQL
-        // request to create its associated document AST.
-        async parsingDidStart(requestContext) {
-          console.log('Parsing started!');
-        },
-  
-        // Fires whenever Apollo Server will validate a
-        // request's document AST against your GraphQL schema.
-        async validationDidStart(requestContext) {
-          console.log('Validation started!');
-        },
-  
-      }
-    },
-  };
-
 const server = new ApolloServer({
     gateway,
     graphqlPath: "ptvapi",
     subscriptions: false,
-    plugins: [prometheusExporterPlugin,gPlugin],
+    plugins: [prometheusExporterPlugin],
     context: ({ req }) => {
         const user = req.user || null;
         const auth = req.headers.authorization || null;
